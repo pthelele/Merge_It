2f92fa4ff1e7a821dcf595256a777c7b9caa86d4
Pale Thelele <pale@tutuka.com>
2018-01-23 15:48:44 +0200

    Getting the files on master branch

--- Merge_IT/src/Logic/logic/Merge_Files.java
+++ Merge_IT/src/Logic/logic/Merge_Files.java
@@ -116,7 +116,7 @@ public class Merge_Files {
 	{
 		// New Unmattering comments.
 		//The method will  have to return the rotated file in pdf format.
-		//Merger file now...
+		//Merger file now... whats going on now.
 	}
 }
 
